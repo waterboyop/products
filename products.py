18ee0d924dbadf6a27a401d9ec1a02b38dca6cfb
Randy Chang <waterboyop@gmail.com>
2022-08-26 14:35:25 +0800

    update new function

--- products.py
+++ products.py
@@ -1,13 +1,18 @@
 products = []
 #cost = 0
 while True:
-    name = input('input the product name: q for exit ')
+    name = input('input the product name: ')
     if name == 'q':
         break
-    price = int(input('please input the product price: '))
+    price = input('please input the product price: ')
 #    p = []
 #    p.append(name)
 #    p.append(price)
     products.append([name, price])
 print(products)
 #print(cost)
+
+with open ('product.csv', 'w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+
