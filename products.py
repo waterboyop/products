6cc5cb88cb942be72af335b324ce05cdc8cb29a0
Randy Chang <waterboyop@gmail.com>
2022-08-29 10:11:23 +0800

    add read version

--- products.py
+++ products.py
@@ -1,5 +1,15 @@
 products = []
-#cost = 0
+
+with open ('product.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品, 價格' in line:
+            continue #繼續 (跳過這一回)
+        name, price = line.strip().split(',')
+        products.append([name, price])
+    print(products)
+
+
+
 while True:
     name = input('input the product name: ')
     if name == 'q':
